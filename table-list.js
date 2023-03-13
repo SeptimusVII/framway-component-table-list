4384d20098657ba3eeafab6d56344bd46ef61e96
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-03-13 18:44:20 +0100

    [feat] add responsive block mode: if the table__container become wider than the global element, switch the display mode from table to blocks [change] remove the default horizontal scroll view of the table. this is now achieved by adding class scroll-x to the table-list [fix] change rem units to em

--- table-list.js
+++ table-list.js
@@ -2,11 +2,40 @@ module.exports = function(app){
     var TableList = Object.getPrototypeOf(app).TableList = new app.Component("table-list");
     // TableList.debug = true;
     TableList.createdAt      = "2.0.0";
-    TableList.lastUpdate     = "2.0.0";
-    TableList.version        = "1";
+    TableList.lastUpdate     = "2.0.2";
+    TableList.version        = "1.1";
     // TableList.factoryExclude = true;
     // TableList.loadingMsg     = "This message will display in the console when component will be loaded.";
     // TableList.requires       = [];
+
+    TableList.prototype.onCreate = function(){
+        var table = this;
+        table.$container = table.$el.find('.table-list__container').length ? table.$el.find('.table-list__container') : false;
+        table.$headline  = table.$el.find('.table-list__headline').length  ? table.$el.find('.table-list__headline')  : false;
+        table.$lines     = table.$el.find('.table-list__line').length      ? table.$el.find('.table-list__line')      : false;
+        table.responsive = (table.responsive !== undefined) ? table.responsive : table.getData('responsive', false);
+
+        if (table.responsive)
+            table.$lines.last().after('<tr class="table-list__line filler"></tr><tr class="table-list__line filler"></tr>');
+
+        if (table.$headline && table.$headline.find('.table-list__cell[data-label]').length) {
+            table.$headline.find('.table-list__cell[data-label]').each(function(){
+                table.$lines.find('.table-list__cell[data-name="'+this.getAttribute('data-name')+'"]').attr('data-label',this.getAttribute('data-label'))
+            })
+        }
+
+
+        return table;
+    };
+
+    TableList.prototype.onResize = function(){
+        var table = this;
+        if (table.responsive) {
+            table.$el.removeClass('mode--block');
+            if (table.$container.width() > table.$el.width())
+                table.$el.addClass('mode--block');
+        }
+    }
 
     $(function(){
         $('body').on('click','.table-list__line.separator',function(e){
