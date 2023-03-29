711222ac881d5d2579a1669d4197b58d6017d7c0
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-03-29 14:49:39 +0200

    [fix] line separator not taking 100% width while in block mode

--- table-list.js
+++ table-list.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var TableList = Object.getPrototypeOf(app).TableList = new app.Component("table-list");
     // TableList.debug = true;
     TableList.createdAt      = "2.0.0";
-    TableList.lastUpdate     = "2.0.2";
-    TableList.version        = "1.1";
+    TableList.lastUpdate     = "2.0.4";
+    TableList.version        = "1.1.1";
     // TableList.factoryExclude = true;
     // TableList.loadingMsg     = "This message will display in the console when component will be loaded.";
     // TableList.requires       = [];
