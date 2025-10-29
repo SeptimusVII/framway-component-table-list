fdf765c1e669dc82c7d009c558c8c2427b1e317f
SeptimusVII <vansteene.quentin@gmail.com>
2025-10-29 18:31:13 +0100

    fix actions display on block mode

--- table-list.js
+++ table-list.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var TableList = Object.getPrototypeOf(app).TableList = new app.Component("table-list");
     // TableList.debug = true;
     TableList.createdAt      = "2.0.0";
-    TableList.lastUpdate     = "2.6.0";
-    TableList.version        = "1.6.2";
+    TableList.lastUpdate     = "2.7.0";
+    TableList.version        = "1.6.3";
     // TableList.factoryExclude = true;
     // TableList.loadingMsg     = "This message will display in the console when component will be loaded.";
     // TableList.requires       = [];
