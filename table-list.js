6d5db2af5688f443d9d2ec1bc474c70658e78a30
SeptimusVII <vansteene.quentin@gmail.com>
2026-08-27 12:25:28 +0200

    add keepOne parameter. disable remove line actions when there is only one line left add confirmRemoveLine parameter

--- table-list.js
+++ table-list.js
@@ -7,7 +7,6 @@ module.exports = function(){
             this.version = "1.0.0";
             this.tpl = utils.getNodeFromString(require('bundle-tpl:./table-list.html')).outerHTML;
             // this.describe();
-            
             this.togglerActionsClick = document.addEventListener('click',(e)=>{
                 let toggler = e.target.closest('.table-list__action--toggler');
                 if (toggler) {
@@ -18,6 +17,9 @@ module.exports = function(){
             this.actionRemoveLineClick = document.addEventListener('click',(e)=>{
                 let action = e.target.closest('.table-list__action[data-action=removeLine]');
                 if (action) {
+                    if (action.closest('.table-list').component.confirmRemoveLine)
+                        if (!window.confirm("Remove line ?"))
+                            return false;
                     let line = action.closest('.table-list__line,tr:not(.table-list__headline)');
                     action.closest('.table-list').component.removeLine(line)  
                 }
@@ -40,46 +42,29 @@ module.exports = function(){
                 }
             });
         }
-
         onCreate(){
-            this.container = this.el.querySelector('.table-list__container');
-            this.headline  = this.el.querySelector('.table-list__headline');
-            this.isTable   = (this.container.nodeName == 'TABLE');
+            this.container     = this.el.querySelector('.table-list__container') ?? false;
+            this.headline      = this.el.querySelector('.table-list__headline')  ?? false;
+            this.isTable       = (this.container.nodeName == 'TABLE');
             if (this.isTable)
                 this.container = this.container.querySelector("tbody");
             this.setLines();
 
-            this.responsive    ??= this.getData('responsive',false);
-            this.minWidthBlock ??= this.getData('minWidthBlock','200px');
-            this.addToolTips   ??= this.getData('addToolTips',false);
-
-
-            if (this.headline && this.headline.querySelectorAll('[data-label]').length && this.lines.length) {
-                this.headline.querySelectorAll('[data-label]').forEach((h_cell)=>{
-                    for(let line of this.lines){
-                        line.querySelectorAll('.table-list__cell,td:not(.table-list__cell)').filter('[data-name="'+h_cell.getAttribute('data-name')+'"]').forEach((l_cell)=>{
-                            // console.log(l_cell);
-                            l_cell.setAttribute('data-label',h_cell.getAttribute('data-label'))
-                            l_cell.setAttribute('title',h_cell.getAttribute('data-label'))
-                        });
-                    }
-                })
-            }
+            this.responsive        ??= this.getData('responsive', false);
+            this.minWidthBlock     ??= this.getData('minwidthblock', false);
+            this.addTooltip        ??= this.getData('addtooltip', true);
+            this.keepOne           ??= this.getData('keepone', true);
+            this.confirmRemoveLine ??= this.getData('confirmremoveline', true);
 
             if (this.responsive){
-                // uncomment if fillers needed
-                // this.lines[this.lines.length-1].after(utils.getNodeFromString('<tr class="table-list__line filler"></tr>'));
-                // this.lines[this.lines.length-1].after(utils.getNodeFromString('<tr class="table-list__line filler"></tr>'));
                 if(this.minWidthBlock)
-                    this.el.style.setProperty('--tablelist-line-minwidth',this.minWidthBlock);
-                let table = this;
-                setTimeout(()=>{table.onResize()},1)
-            } else {
-                this.onResize();
+                    for(var line of this.lines)
+                        line.style.minWidth = this.minWidthBlock
             }
 
-            this.log('onCreate')
+            // console.log(this);
         }
+
         convertTooltips() {
             this.el.querySelectorAll('.table-list__action').forEach((el)=>{
                 if (el.getAttribute('title')) {
@@ -90,7 +75,7 @@ module.exports = function(){
             utils.adjustTooltips()
         }
         getLines(){
-            return this.el.querySelectorAll('.table-list__line,tr:not(.table-list__headline)');
+            return this.el.querySelectorAll('.table-list__line,tr:not(.table-list__headline):not(.separator)') ?? false;
         }   
         setLines(){
             this.lines = this.getLines();
@@ -98,11 +83,27 @@ module.exports = function(){
                 this.convertTooltips();
             if (this.el.classList.contains('lastColSticky'))
                 this.setStickyObservers();
+            if(this.headline && this.headline.querySelector('[data-label]') && this.lines.length)
+                this.setLinesTitles();
+            if (this.keepOne) {
+                this.el.classList.remove('no-removeLine');
+                if (this.lines.length <= 1)
+                    this.el.classList.add('no-removeLine');
+            }
+        }
+        setLinesTitles(){
+            for(var thcell of this.headline.querySelectorAll('.table-list__cell,:is(td,th):not(.table-list__cell)').filter('[data-label]')){
+                for(var line of this.lines){
+                    let cell = line.querySelector('[data-name="'+thcell.getAttribute('data-name')+'"]');
+                    if (cell) {
+                        cell.setAttribute('data-label',thcell.getAttribute('data-label'));
+                        cell.setAttribute('title',thcell.getAttribute('data-label'));
+                    }
+                }
+            }
         }
         setStickyObservers(){
-            console.log('setStickyObservers');
             for( var line of this.lines){
-                console.log(line);
                 utils.monitorStick(line.querySelector('td:last-child,.table-list__cell:last-child'), 'is-pinned', '0px -1px 0px 0px');
             }
         }
@@ -111,7 +112,7 @@ module.exports = function(){
             this.setLines();
             return this;
         }
-        addLine(data = false, attributes = false){
+        addLine(data = false, attributes = false){ 
             var line = document.createElement(this.isTable?'tr':'div');
             var cellTag = this.isTable?'td':'div';
             var columns = [];
@@ -141,16 +142,13 @@ module.exports = function(){
             this.setLines();
             return line;
         }
-
         onResize(){
             if (this.responsive) {
                 this.el.classList.remove('mode--block');
                 if (this.container.clientWidth > this.el.clientWidth)
                     this.el.classList.add('mode--block');
             }
-            this.log('resize');
         }
     }
     return TableList;
 }
-
